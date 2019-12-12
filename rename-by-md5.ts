d8939afbff7b2eb58078310f07e479428a9b036b
masx200 <34191203+masx200@users.noreply.github.com>
2019-12-12 20:30:37 +0800

    文件使用md5重命名,并且防止文件名中出现重复多次md5值

--- rename-by-md5.ts
+++ rename-by-md5.ts
@@ -1,5 +1,48 @@
+const extention = "webp";
 import find from "find";
-import md5file from "md5-file";
 import fs from "fs";
+import md5file from "md5-file";
+import path from "path";
+const dirpath = path.resolve("D:\\baidupandownload");
+function md5FileAsPromised(filename: string): Promise<string> {
+  return new Promise(function(resolve, reject) {
+    md5file(filename, function(err, hash) {
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(hash);
+      }
+    });
+  });
+}
 const fspromise = fs.promises;
-console.log([find, md5file, fspromise]);
+// console.log([find, md5file, fspromise]);
+
+const extreg = new RegExp("." + extention + "$");
+
+console.log([extention, dirpath]);
+function findfiles(pattern: string | RegExp, root: string): Promise<string[]> {
+  return new Promise((s, j) => {
+    find
+      .file(pattern, root, files => {
+        s(files);
+      })
+      .error(e => {
+        j(e);
+      });
+  });
+}
+findfiles(extreg, dirpath).then(files => {
+  console.log(files);
+  files.forEach(file => {
+    md5FileAsPromised(file).then(hash => {
+      console.log([file, hash]);
+      const newfilename = file
+        .replace(new RegExp("-" + hash, "g"), "")
+        .replace(extreg, `-${hash}.${extention}`);
+      fspromise.rename(file, newfilename).then(() => {
+        console.log(["rename success", newfilename]);
+      });
+    });
+  });
+});
